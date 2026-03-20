93baa859798b0ac847ce861cff53fc56642bda48
hency-20 <bodarhensi@gmail.com>
2026-03-20 18:31:34 +0530

    Initial project setup

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <div className='font-bold bg-blue-500'> Hello World</div>
+    </>
+  )
+}
+
+export default App
